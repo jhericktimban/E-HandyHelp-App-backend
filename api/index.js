c8257a789c808f202704839e3b0acad8cc0496c7
Jherick Timban <jrickvillaqwe@gmail.com>
2025-01-21 16:21:42 +0800

    updated otp api

--- api/index.js
+++ api/index.js
@@ -1384,7 +1384,9 @@ const generateOTP = () => {
 async function sendOTPEmail(email, otp) {
   try {
     const transporter = nodemailer.createTransport({
-      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 587, 
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
